db24a423d1c19230573f8a6c23438e2d6679a811
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-10 15:46:39 -0700

    Acorrected file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		while (num_2 < 58)
 		{
-			if (num_1 != num_2 && num_2 > num_1 && num_1 != 56 && num_2 != 57)
+			if (num_1 != num_2 && num_2 > num_1 && (num_1 != 56 && num_2 != 57))
 			{
 				putchar(num_1);
 				putchar(num_2);
