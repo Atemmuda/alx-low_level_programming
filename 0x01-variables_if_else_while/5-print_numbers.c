a3cc813649801b4dc39c5225db686de8d99a69c5
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-10 13:29:54 -0700

    Added file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * num - holds the number between 0 an 10
+ *
+ * Return: 0 after printing the funtion
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num <= 10)
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
