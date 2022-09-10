08be9a01636d0d99afaf0534d4586a99013abb71
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-10 14:12:18 -0700

    Added file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * num - holds the number between 0 an 10
+ *
+ * Return: 0 after printing the funtion
+ */
+int main(void)
+{
+	int num;
+
+	num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		putchar((int) ',');
+		putchar((int) ' ');
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
