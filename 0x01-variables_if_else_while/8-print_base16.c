11d74bd5075577003ae0c21d3209b83e730c1819
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-10 14:03:30 -0700

    Added file 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * c - hold the curent value of the alphabet
+ * num - hold the number part of number
+ *
+ * Return: 0 after printing the funtion
+ */
+int main(void)
+{
+	char ch;
+	int num;
+
+	ch = 'a';
+	num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+
+	while (ch >= 'f')
+	{
+		putchar((int) ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
