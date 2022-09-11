c6cfb914aab4f14faa9f7c390c0fa54564b1c6db
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-11 05:55:12 -0700

    ded file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	putchar('\n');
+	return (0);
+}
