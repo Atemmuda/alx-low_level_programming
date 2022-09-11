0bee0c0b616f242068a6eede604f5e534469c65c
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-11 09:00:45 -0700

    Added file 6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program
  *
- * Return: ALways 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program
  *
- * Return - whan all is done correctly give me 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
