35298e09ec66571b94c5121f9a7b8fb5dfeaeadb
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-11 08:55:16 -0700

    Added file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
- * return - whan all is done correctly give me 0
+ * Return - whan all is done correctly give me 0
  */
 
 int main(void)
+
 {
-	printf("Size of char: %d byte(s)\n" char);
+	int i;
+	long int li;
+	long long int lli;
+	char ch;
+	float fl;
+
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(ch));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(fl));
 	return (0);
 }
 
