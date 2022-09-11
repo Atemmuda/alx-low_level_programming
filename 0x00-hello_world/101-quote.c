5cbceb3b7dbb9874fd4298275409308e712a9f88
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-11 08:42:04 -0700

    Added file 6-size.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point of the program
+ *
+ * error - array of error message.
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char error[] = "and that piece of art is useful\" - Dora Korpar
+			2015-10-19 \n";
+	write(2, error, 59);
+
+	return (1);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * return - whan all is done correctly give me 0
+ */
+
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n" char);
+	return (0);
+}
+
