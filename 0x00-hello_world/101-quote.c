667e340d189e9acb9eec82ffe3c49e7880c2e6dc
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-11 09:25:38 -0700

    Added file 6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
  */
 int main(void)
 {
-	char error[] = "and that piece of art is useful\" - Dora Korpar
-			2015-10-19 \n";
+	char error[11];
+       error = ["and ", "that ", "piece ", "of ", "art " , "is ", "useful\" " , "-", " Dora", " Korpar", " 2015-10-19", "\n"];
 	write(2, error, 59);
 
 	return (1);
