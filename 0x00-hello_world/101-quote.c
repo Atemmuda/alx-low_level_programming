045d9966647615aafb42a82f3b2ca8be06775152
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-11 10:16:36 -0700

    Added file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-	char error[] =  {"and that piece of art is useful\"""- Dora Korpar 2015-10-19\n"};
+	char error[] =  {"and that piece of art is useful\""" - Dora Korpar, 2015-10-19\n"};
 	write(2, error, 59);
 
 	return (1);
