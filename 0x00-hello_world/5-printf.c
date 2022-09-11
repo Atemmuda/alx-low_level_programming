21a851aeede8bed70939ce2689d22a0a2df5c7af
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-11 06:02:17 -0700

    Added file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
