406b4f326e8f875bef316564a75a50fee744dfd5
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-12 13:26:33 -0700

    Added file 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,44 +5,18 @@
 /**
  * main - Entry point
  *
- * num1 - holds the number between 0 an 10
- * num2 - holds  the value of number between 0 and 10
- *
  * Return: 0 after printing the funtion
  */
 int main(void)
 {
-	int num_1;
-	int num_2;
-	int num_3;
+	int i;
+	int j;
 
-	num_1 = 48;
-	num_2 = 49;
-	num_3 = 50;
-
-	while (num_1 < 56)
+	for (i = 0; i < 100; i++)
 	{
-		while (num_1 < 57)
-		{
-			while (num_3 < 58)
-			{
-				if (num_1 != num_2 && num_2 != num_3 && num_1 != num_3
-						&& num_3 > num_2 && num_2 > num_1)
-				{
-					putchar(num_1);
-					putchar(num_2);
-					putchar(num_3);
-					if (num_1 != 55 && num_2 != 56 && num_3 != 57)
-					{
-						putchar((int) ',');
-						putchar((int) ' ');
-					}
-					num_3++;
-				}
-			}
-			num_2++;
-		}
-		num_1++;
+		putchar(i);
+		putchar(i);
+		putchar('\n');
 	}
 
 	putchar('\n');
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * num1 - holds the number between 0 an 10
+ * num2 - holds  the value of number between 0 and 10
+ *
+ * Return: 0 after printing the funtion
+ */
+int main(void)
+{
+	int num_1;
+	int num_2;
+	int num_3;
+
+	num_1 = 48;
+	num_2 = 49;
+	num_3 = 50;
+
+	while (num_1 < 56)
+	{
+		while (num_1 < 57)
+		{
+			while (num_3 < 58)
+			{
+				if (num_1 != num_2 && num_2 != num_3 && num_1 != num_3
+						&& num_3 > num_2 && num_2 > num_1)
+				{
+					putchar(num_1);
+					putchar(num_2);
+					putchar(num_3);
+					if (num_1 != 55 && num_2 != 56 && num_3 != 57)
+					{
+						putchar((int) ',');
+						putchar((int) ' ');
+					}
+					num_3++;
+				}
+			}
+			num_2++;
+		}
+		num_1++;
+	}
+
+	putchar('\n');
+	return (0);
+}
