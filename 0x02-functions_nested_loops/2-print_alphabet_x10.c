0f7a3e9e0a5864d923e5e1b15bf9ecca9f137364
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-14 07:24:45 -0700

    Added file 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints all the alphabets, a-z ten times
+ *
+ * Return: always 0 success
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	int index;
+
+	alpha = 'a';
+
+	for (index i = 0; i < 10; i++)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar((int) alpha);
+			alpha++;
+		}
+		_putchar((char) 10);
+
+	}
+}
