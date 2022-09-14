75732b328b36fea6aa2c313b85d04d06a1f2c1f5
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-14 09:07:42 -0700

    added file 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - makes a number positive even if negative
+ * @a:integer value
+ *
+ * Return: the the last digit.
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+
+	return (a);
+}
