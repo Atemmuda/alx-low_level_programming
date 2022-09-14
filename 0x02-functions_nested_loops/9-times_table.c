c9381191c963811b6e935346e6624fcad11dd304
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-14 09:59:28 -0700

    Added file 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - adds two numbers anr produce the answer
+ * @a:first integer value
+ * @b:second integer value
+ *
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 1; i <=9 i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			k = i * j;
+			_putchar((char) k);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
