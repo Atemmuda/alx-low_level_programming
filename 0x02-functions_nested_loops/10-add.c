26c3d3d2f246a21edd75b1ab0faad21871cfa0e3
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-14 08:41:28 -0700

    added file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two numbers anr produce the answer
+ * 
+ * @ int: acceptable data type
+ *
+ * Return: the summation
+ */
+int add(int a, intb)
+{
+	return (a + b);
+}
