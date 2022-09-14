f182e3a81f9917b6049da71eec45855614b9f75d
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-14 10:06:39 -0700

    Added file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,12 @@ void times_table(void)
 	int j;
 	int k;
 
-	for (i = 1; i <=9; i++)
+	for (i = 1; i <= 9; i++)
 	{
 		for (j = 1; j <= 9; j++)
 		{
 			k = i * j;
-			_putchar((char) k);
+			_putchar(k);
 			_putchar(',');
 			_putchar(' ');
 		}
