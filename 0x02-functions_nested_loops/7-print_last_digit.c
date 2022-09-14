1ce94cf396d7e2fbbb2f67e254fd788564b0b689
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-14 08:51:11 -0700

    added file 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers anr produce the answer
+ * @a:first integer value
+ *
+ * Return: the the last digit.
+ */
+int print_last_digit(int a)
+{
+	return (a + b);
+}
