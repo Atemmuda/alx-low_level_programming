7c9fcef7dae04a78c8bcf2121484ca8c9ca169e8
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-15 18:04:55 -0700

    Corrected file 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
-*
 * main - causes an infinite loop
 * Return: 0
 */
@@ -10,6 +9,7 @@ int main(void)
         int i;
 
         printf("Infinite loop incoming :(\n");
+
 /*
 *	i = 0; 
 *
