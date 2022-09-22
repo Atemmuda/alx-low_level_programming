22a29948891d91d9af812e3448ee88c06ab990cc
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-22 15:57:32 -0700

    Added file 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strcat -function concatenates two strings
+* @dest: resulting/destination string
+* @src: we copy the string from this variable
+* @n: number of characters to copy
+*
+* Return: a pointer to the two concatenated strings
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] != '\0')
+		{
+			dest[i] = src[i];
+		}
+
+		for ( ; i < n; i++)
+		{
+			dest[i]  = '\0';
+		}
+	}
+}
