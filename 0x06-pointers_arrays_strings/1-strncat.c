32f24a9492de605048e64f2ca634a1bac461d492
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-22 17:48:46 -0700

    file 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,8 +13,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 
-	for(i = 0; src[i] != '\0'; i++)
-		dest[i + n] = src[i];
+	for(i = 0; i < n && src[i] != '\0'; i++)
+
+		dest[i] = src[i];
+		//dest[i + n] = src[i];
 	
 	dest[i + n] = '\0';
 
