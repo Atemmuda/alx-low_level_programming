11f31e934d00406dfd53c3a5cb2b24c2d6ed507c
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-22 17:01:00 -0700

    Added file 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+/**
+ * reverse_array - reverses an array of integers
+ * @a: input array of integers
+ *
+ * @n: number of elements in array
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, end, holder;
+
+	end = n - 1;
+
+	for (i = n; i >= 0; i--)
+	{
+		_putchar((char) a[i]);
+	}
+}
