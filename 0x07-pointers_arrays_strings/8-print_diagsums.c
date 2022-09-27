8249cf196215c61c08e6867c4084d32da00e7666
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-27 07:11:17 -0700

    Added file 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,5 +9,22 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int i;
+	int j;
+	int sum;
+
+	sum = 0;
+	for(i = 0; i < size; i++)
+	{
+		for(j = 0; j < size; j++)
+		{
+			if(i == j)
+			{
+				sum += a[i][j];
+			}
+		}
+	}
+	_putchar(sum);
+
 	return (0);
 }
