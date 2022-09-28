27e3fdd55c7003c6997ec1cf19cdfb8e7a7b7992
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-27 20:00:32 -0700

    Added file 2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,13 +3,12 @@
 /**
  * *_memset - fills the first n bytes of memory are pointed:
  * to by s with the constant byte b
- * @*s: character set pointer
+ * @s: character set pointer
  * @b: constant character byte
  * @n: first n byte in memory to fill
  *
  * Return: A pointer to to the memory area s
  */
- 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * *_memcpy - fills the first n bytes of memory are pointed to by s with the constant byte b
- * @*dest: pointer of the destination string
- * @*src: pointer to the source string
+ * _memcpy - fills the first n bytes of memory are pointed to by s with the constant byte b
+ * @dest: pointer of the destination string
+ * @src: pointer to the source string
  * @n: first n byte in memory to fill
  *
  * Return: A pointer to dest
  */
- 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+		dest[j] = src[j];
+
 	return (dest);
 }
