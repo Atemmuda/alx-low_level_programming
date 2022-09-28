18bd174160097fe8d98bc35dfda98e83fe4ec07b
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-28 11:00:24 -0700

    Added file 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - finds the factorial of a given number
+ * @s: the upper number to find its factorial
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
