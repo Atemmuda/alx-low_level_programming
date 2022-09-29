1daea5205ba5cdd7feeb0d2cbe2855e9c01d9326
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-09-28 19:27:27 -0700

    Added file 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_put_recursion(s + 1);
+}
