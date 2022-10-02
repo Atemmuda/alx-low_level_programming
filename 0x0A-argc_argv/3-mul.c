35b990878f3408cd68c6e6bca4eb0c00670e3705
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-02 16:40:51 -0700

    File 3 added

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,11 +13,12 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc > 2)
+	if (argc > 2 || argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
