4c96dbb0e2c3f8960890d25ab09a060bf6dbf276
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-01 20:38:38 -0700

    Added file 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Print the name of the file
+ * if you rename the program,it will print the new name 
+ * without having to compile again.
+ * You should not remove the path before the name of the program
+ * @argv: The number of elements passed at runtime
+ * @argc: Character array of the element passed
+ *
+ * Return: 0 always success
+ */
+int main(int argv, char *argc[])
+{
+	_putchar('.');
+	_putchar('/');
+	_putchar(argc[0]);
+
+	return (0);
+}
+
