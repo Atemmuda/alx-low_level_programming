9e0d3d4cf35206b08652fdec0da639c5d6ff7324
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-01 22:50:13 -0700

    Added file 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * Assuming the numbers and the results are integers
+ * @argv: The number of elements passed at runtime
+ * if > 2 print error followed by a new line and return 1
+ * @argc: Character array of the element passed
+ *
+ * Return: value of argv[1] and argv[2]
+ */
+int main(int argc, char *argv[])
+{
+
+	if (argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (argv[1] * argv[2]);
+}
