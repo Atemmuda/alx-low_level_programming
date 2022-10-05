b29797cb649a22a9cbe632286432b46488c1324c
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-04 22:04:51 -0700

    Added file 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,13 +18,14 @@ int main(int argc, char *argv[])
 
 	sum = 0;
 
-	if (argv[1] == NULL)
+	if (argc <= 1)
 	{
 		printf("%d\n", 0);
+		return (0);
 	}
 	else
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i <= (argc - 1); i++)
 		{
 			for (j = 0; j < argv[i][j]; j++)
 			{
