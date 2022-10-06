9015e882e80cdd2008a62d05a14b55e4e3d21f58
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-05 19:33:30 -0700

    Added file 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *array =(char *) malloc(sizeof(char) * size);
+	char *array = (char *) malloc(sizeof(char) * size);
 
-	if (size == 0 || array == '\0')
+	if (size == 0 || *array == '\0')
 	{
 		return (0);
 	}
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
