ab235b88f4a83bb5025159a4337538a054c3b904
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-11 20:17:28 -0700

    Added file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,9 +10,22 @@
  * Description: Creation of a user defined dog with the
  * above properties
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - defining a new dog of type dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
+
