df1b5249e4c6525de89e685017b04563c569164c
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-12 16:07:17 -0700

    Added file 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name of the string characters
+ * passed to f
+ * @name: string to print
+ * @f: the functio that print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
