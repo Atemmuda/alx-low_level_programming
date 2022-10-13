e5a0a972aaab6048ff48ee7200fee62c724754c1
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-12 17:01:50 -0700

    Added file changes

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates over array of of the size
+ * @array: array to iterate over
+ * @size: size of the array, hence the limit of iteration
+ * @action: function pointer which take in integer
+ * hence array[i]
+ *
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !size || !action)
+		return;
+	
+	for (i = o; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
