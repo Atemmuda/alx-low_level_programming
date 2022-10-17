ec7bdc8974af0b7ec97871a14a8355060187a991
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-17 00:07:12 -0700

    Added code to file 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include "variadic_function.h"
+
+/**
+ * sum_them_all - sum the variable number of elements
+ * @n: the number of elements
+ *
+ * Return: integer value of the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i, sum;
+
+	if (n == 0)
+		return (0);
+
+	var_start(list, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += var_arg(list, int);
+
+	var_end(list);
+
+	return (sum);
+}
