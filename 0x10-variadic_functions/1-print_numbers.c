0a88e23bf8492bdf10a252afd4ca0172994e71a4
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-17 15:07:16 -0700

    Added file 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, unsigned int n, ...)
 		if (separator && i == 0)
 			printf("%d", va_arg(list, int));
 		else
-			printf("%d%s", va_arg(list, int), separator);
+			printf("%d%s", separator, va_arg(list, int));
 	}
 
 	va_end(list);
