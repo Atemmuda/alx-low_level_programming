5f980251f113234df5d8e97cbcf4a815a883c579
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-24 07:52:10 -0700

    Added file 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,2 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
 
+/**
+ * print_list - print all the elements of a list_t list
+ * @h: pointer to the structure list_t
+ *
+ * Return: the size of the list
  */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+				printf("[%u] %s", h->len, h->str);
+		s++;
+	}
+
+	return (s);
+}
