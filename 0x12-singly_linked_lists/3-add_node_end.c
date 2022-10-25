1c6afd637eff29162dd6954a3e990122a19d7e83
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-24 22:40:42 -0700

    Added file 3 correction

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,6 +12,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_element;
+	list_t *temp = *head;
 	unsigned int len = 0;
 
 	while (str[len])
@@ -23,8 +24,18 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_element->str = strdup(str);
 	new_element->len = len;
-	(*head) = new_element;
-	new_element->next = (*head);
+	new_element->next = NULL;
 
-	return (*head);
+	if (*head == NULL)
+	{
+		*head = new_element;
+		return (new_element);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new_element;
+
+	return (new_element);
 }
