71f65bfdc4d31eb4721002b96ac21b51a7c615ea
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-25 20:48:28 -0700

    Added file 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the gead of the list
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
