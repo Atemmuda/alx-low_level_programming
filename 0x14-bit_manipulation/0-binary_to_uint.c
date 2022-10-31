83bd3e586e77e772c56249e12c7807dcdd142e7a
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-30 23:36:50 -0700

    Added file 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a given binary to an decimal value
+ * @b: pointer to binary string
+ *
+ * Return: The decimal value or 0 when b is NULL or b[i]
+ * is not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, decimal;
+
+	if (b == NULL)
+		return (0);
+	i = 0;
+	while (b[])
+}
