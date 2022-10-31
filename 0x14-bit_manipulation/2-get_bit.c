3f18697630696c8385a0e70497afaef6d34f4d8e
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-31 16:49:34 +0000

    Added file 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,17 +9,17 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
+        unsigned int i;
 
-	if (index > sizeof(n) * 8)
-	{
-		return (-1);
-	}
+        if (index > sizeof(n) * 8)
+        {
+                return (-1);
+        }
 
-	for (i = 0; i < index; i++)
-	{
-		n = n >> i;
-	}
+        for (i = 0; i < index; i++)
+        {
+                n = n >> i;
+        }
 
-	return (n & 1);
+        return (n | 1);
 }
