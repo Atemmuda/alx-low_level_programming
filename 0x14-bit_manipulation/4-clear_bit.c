af252c6c17f489bbd7e22eadb3269f5ee3d17b16
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-31 21:21:26 -0700

    Betty corrected 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,8 +11,8 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        if (index > sizeof(*n) * 8)
-                return (-1);
-        *n = (*n & ~(1 << index));
-        return (1);
+	if (index > sizeof(*n) * 8)
+		return (-1);
+	*n = (*n & ~(1 << index));
+	return (1);
 }
