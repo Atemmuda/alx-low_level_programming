c409c55c019c91dd43ce8074db66d87975210373
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-11-04 23:00:55 -0700

    corrected file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,23 +24,25 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
+	file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	file_des = open(filename, O_CREAT | O_RDWR | O_TRUNC);
 	if (file_des == -1)
 		return (-1);
 
 	while (text_content[text_size])
 		text_size++;
 
+	if (text_content == NULL)
+	{
+		close(file_des);
+		return (-1);
+	}
+
 	wr = write(file_des, text_content, text_size);
 
-	if (wr < 0)
+	if (wr == -1)
 		return (-1);
 
-	if (close(file_des) < 0)
-		return (-1);
-
+	close(file_des);
 	return (1);
 }
