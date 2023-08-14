605e61a99bf8b74829cd3dc8a89b0c14acb35be7
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-08-14 07:45:02 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the program
+ *
+ * prints first 10 base ten numbers
+ *
+ * Return: zero(0) for success
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar((int)n);
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
