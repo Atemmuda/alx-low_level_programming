7cbcefadc6bbb120dba8e9192006bbf289289545
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-08-22 01:51:19 +0000

    added added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string.
+ * @s: pointer to the string
+ *
+ * Return: integer length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len, i;
+	char pp;
+
+	pp = s[0];
+
+	for (i = 1; pp != '\0'; i++)
+	{
+		len++;
+		pp = s[i];
+	}
+
+	return (len);
+}
+
