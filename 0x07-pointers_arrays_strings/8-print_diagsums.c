f77348ca2ec4862ae0fc7d1a186beeb77990604a
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-08-29 03:38:47 +0000

    added Final work on file #8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,56 +1,38 @@
 #include "main.h"
 #include <stdio.h>
 
+
 /**
  * print_diagsums: prints the sum od the daigonal
  * of the n*m dimanentional array
- * 
+ *
  * @a: pointer to the array
  * @size: the size of the array
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum;
+	int i, j, row_major, sum_left, sum_right;
 
-	i = 0;
+	sum_left = 0;
+	sum_right = 0;
+
 	j = 0;
-	sum = 0;
-
-	printf("diagsum started\n");
-
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (; j < size; j++)
-		{
-			if (i == j)
-				sum += a[j];
-		}
+		row_major = (size * i) + j;
+		sum_left += *(a + row_major);
+		j++;
 	}
-	_putchar((char) sum);
+	printf("%d, ", sum_left);
 
-	return;
+	j = size - 1;
+	row_major = 0;
+	for (i = 0; i < size; i++)
+	{
+		row_major = (size * i) + j;
+		sum_right += *(a + row_major);
+		j--;
+	}
+	printf("%d\n", sum_right);
 }
 
-/**
- * main - Entry point
- *
- * Return: 0 on success
- */
-int main(void)
-{
-    int c3[3][3] = {
-        {0, 1, 5},
-        {10, 11, 12},
-        {1000, 101, 102},
-    };
-    int c5[5][5] = {
-        {0, 1, 5, 12124, 1234},
-        {10, 11, 12, 123521, 12512},
-        {1000, 101, 102, 12545, 214543435},
-        {100, 1012451, 11102, 12545, 214543435},
-        {10, 12401, 10452, 11542545, 1214543435},
-    };
-    print_diagsums((int *)c3, 3);
-    print_diagsums((int *)c5, 5);
-    return (0);
-}
