d3eee14bef720f546e3a33e9c19a7611da3e614e
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-09-04 12:55:13 +0000

    added Added file 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _atoi - a function that convert a string to an integer.
  * @s: pointer to the string
@@ -11,15 +12,7 @@ int _atoi(char *s)
 	int i, sign, plus_count, minus_count, num_for_str;
 
 	i = 0;
-
-	/* Leading white spaces */
 	sign = 1;
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-
 	num_for_str = 0;
 	plus_count = 0;
 	minus_count = 0;
@@ -41,9 +34,7 @@ int _atoi(char *s)
 			minus_count += 1;
 		}
 		if (s[i] >= '0' && s[i] <= '9')
-		{
 			num_for_str = num_for_str * 10 + s[i] - '0';
-		}
 		else if (s[i] != '\0')
 			i++;
 		else
@@ -53,36 +44,7 @@ int _atoi(char *s)
 		}
 	}
 	if (plus_count < minus_count && minus_count % 2 != 0)
-	{
 		sign = -1;
-	}
 	return (num_for_str * sign);
 }
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int nb;
-
-    nb = _atoi("98");
-    printf("%d\n", nb);
-    nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-    printf("%d\n", nb);
-    nb = _atoi("214748364");
-    printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    printf("%d\n", nb);
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);
-    return (0);
-}
