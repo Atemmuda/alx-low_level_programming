f43972a23a9b1d095f3569ee0138b9b879ff03c6
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-09-15 21:22:27 +0000

    added Added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "mina.h"
+/**
+ * add - a function that adds two integers and returns the result.
+ * @a: first integer value
+ * @b: second integer value
+ *
+ * Return: the sunm of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
