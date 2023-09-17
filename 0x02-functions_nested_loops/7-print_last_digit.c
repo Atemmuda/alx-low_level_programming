80aa65f4e9c909abe665829ad3b484d559afef61
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-09-17 16:37:54 +0000

    added Corrected 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,22 +12,11 @@ int print_last_digit(int a)
 	int last_digit;
 
 	last_digit = a % 10;
-	if(last_digit < 0)
+	if (last_digit < 0)
 	{
 		last_digit = 0 - last_digit;
 	}
 	_putchar(last_digit + '0');
-	return ( last_digit);
+	return (last_digit);
 }
 
-int main(void)
-{
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
-}
