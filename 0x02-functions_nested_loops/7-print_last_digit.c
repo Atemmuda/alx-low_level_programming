3895bdfff9122d28b2214890cdac4093ba40b0dd
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-09-17 16:20:45 +0000

    added Added 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,25 @@
  */
 int print_last_digit(int a)
 {
-	return (a % 10);
+	int last_digit;
+
+	last_digit = a % 10;
+	if(last_digit < 0)
+	{
+		last_digit = 0 - last_digit;
+	}
+	_putchar(last_digit + '0');
+	return ( last_digit);
 }
 
+int main(void)
+{
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
