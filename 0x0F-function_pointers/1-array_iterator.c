cae9d9f3c52604aaa654790eefc90bb2323d2e40
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-11-09 06:01:17 +0000

    added file 2-int_index.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#define NULL 0
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes function as a parameter
+ * on each element of array
+ * @array: input array of ints to use as function parameters
+ * @size: size of int array
+ * @action: pointer to function to run with each element of array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	if (array != NULL action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
+}
