c9953d9b3c0949f877a3516cb0ac36ead548b5da
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-11-09 05:37:50 +0000

    added file 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#define NULL 0
+
+/**
+ * print_name - function that prints a name
+ * @name: input string containing name
+ * @f: pointer to function with input string
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		(*f)(name);
+	else
+		return;
+}
