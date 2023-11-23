b2ba88fd85522f048d94a730215695499233bb6f
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-11-23 08:27:35 +0000

    Corrected file 101 with the segmentation fault

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,18 @@
-global  main
-        extern  printf
+section .data
+    format db 'Hello, Holberton', 10 ; The 10 represents a newline character
+
+section .text
+    global main
+    extern printf
+
 main:
-        mov edi, format
-        xor eax, eax
-        call printf
-        mov eax, 0
-format: db 'Hello, Hoblberton\n',0
+    ; Set up the stack for printf function call
+    sub rsp, 8      ; Align the stack to a 16-byte boundary
+    lea rdi, [format] ; Load the address of the format string into rdi
+    xor eax, eax    ; Clear EAX register (return value of printf)
+    call printf     ; Call printf function
+    add rsp, 8      ; Restore the stack pointer
+
+    ; Exit the program
+    mov eax, 0      ; Return 0 status
+    ret             ; Return from the main function
